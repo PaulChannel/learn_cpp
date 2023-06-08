885592328b9b0ed20a0d7126c3356a022e11c8ee
yougen.hu <paul_hu_use@126.com>
2023-06-08 19:52:01 +0800

    init

---
+++ syntax.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Paul on 2023/6/8.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+}
+
+class Line {
+public:
+    int width{};
+    int length{};
+    int high{};
+    Line();
+
+    void setWidth(int width);
+};
+
+Line ::Line() {
+    cout << "this is constructor !";
+}
+
+void Line::setWidth(int width) {
+    width = width;
+}
